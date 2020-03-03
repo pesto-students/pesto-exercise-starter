65609317c42136cb32c11f86cc6a60f090c3abc5
anshul0888 <anshulsharma0888@gmail.com>
2020-03-03 19:49:30 +0530

    done

--- js-exercises/duplicate-letters/duplicateLetters.js
+++ js-exercises/duplicate-letters/duplicateLetters.js
@@ -1,8 +1,15 @@
-
 function duplicateLetters(...args) {
-  return args;
+  const str = args[0];
+  let count = 0;
+
+  str.split("").forEach((item, index, array) => {
+    if (array.lastIndexOf(item) !== index) {
+      count++;
+    }
+  });
+
+  const result = count ? console.log("found ", count) : false;
+  return result;
 }
 
-export {
-  duplicateLetters,
-};
+export { duplicateLetters };
