f57c2f5d3a7844c4572fa17439c81b4795ecd4c1
Zohran Danish <zohranmohammaddanish@gmail.com>
2020-03-03 19:39:08 +0530

    Complete

--- js-exercises/sum-fibs/sumFibs.js
+++ js-exercises/sum-fibs/sumFibs.js
@@ -1,7 +1,13 @@
 function sumFibs(n) {
-  for(let i=0; i<=n;i++) {
-    console.log(fib(n)%2===0 ? null : fib(n));
+  let i =0;
+  let sum = 0;
+  while(fib(i)< n) {
+    if(fib(i)%2!=0) {
+      sum = sum+fib(i);
+    }
+    i++;
   }
+  return sum;
 }
 
 const fib = n => n <= 1 ? n : fib(n-1) + fib(n-2); 
--- js-exercises/sum-primes/sumPrimes.js
+++ js-exercises/sum-primes/sumPrimes.js
@@ -1,5 +1,19 @@
-function sumPrimes() {
+function sumPrimes(n) {
+  let sum = 0;
+  for(let i = 2; i <= n ; i++ ) {
+    if(isPrime(i)) {
+      sum = sum + i;
+    }
+  }
+  return sum;
+}
 
+function isPrime(n) {
+  for(let i = 2; i < n ; i++) {
+    if(n%i===0)
+      return false;
+  }
+  return true;
 }
 
 export {
