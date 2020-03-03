16e178cc0b88c8e84374fad16f2291cd1b702191
Zohran Danish <kathyayani098@gmail.com>
2020-03-03 17:24:53 +0530

    sumFibs

--- js-exercises/sum-fibs/sumFibs.js
+++ js-exercises/sum-fibs/sumFibs.js
@@ -1,6 +1,10 @@
-function sumFibs() {
-
+function sumFibs(n) {
+  for(let i=0; i<=n;i++) {
+    console.log(fib(n)%2===0 ? null : fib(n));
+  }
 }
+
+const fib = n => n <= 1 ? n : fib(n-1) + fib(n-2); 
 
 export {
   sumFibs,
