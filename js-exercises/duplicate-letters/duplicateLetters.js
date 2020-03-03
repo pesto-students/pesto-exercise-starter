fae49cebd1fb9fda0ddb6a6e3e7f46a79bbabae2
Rajiv K.R <rajiv270992@gmail.com>
2020-03-03 17:59:49 +0530

    solving duplicateletters

--- js-exercises/duplicate-letters/duplicateLetters.js
+++ js-exercises/duplicate-letters/duplicateLetters.js
@@ -1,8 +1,17 @@
+function duplicateLetters(inputStr = '') {
+  const eachCharCount = {};
+  let maxCount = 0;
 
-function duplicateLetters(...args) {
-  return args;
+  inputStr.split('').forEach(char => {
+    if (eachCharCount[char] !== undefined) {
+      eachCharCount[char] += 1;
+      if (eachCharCount[char] > maxCount) maxCount = eachCharCount[char];
+    } else {
+      eachCharCount[char] = 1;
+    }
+  });
+
+  return maxCount === 0 ? false : maxCount;
 }
 
-export {
-  duplicateLetters,
-};
+export { duplicateLetters };
