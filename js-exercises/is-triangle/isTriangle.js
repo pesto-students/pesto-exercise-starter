a58f6f6d54708dd8a60ec74f5c220a056719b293
nihal1993 <jaimini.nihal@gmail.com>
2020-03-03 19:12:11 +0530

    commitng

--- js-exercises/is-triangle/isTriangle.js
+++ js-exercises/is-triangle/isTriangle.js
@@ -1,5 +1,16 @@
-function isTriangle() {
 
+function isTriangle (a, b, c) {
+  if ((a + b <= c) || (a + c <= b) || (b + c <= a)) {
+    return false;
+  }
+  else {
+    if ((a <= 0) || (b <= 0) || (c <= 0)) {
+    return false;
+  }
+    else {
+    return true;
+    }
+  }
 }
 
 export {
--- js-exercises/sum-all/sumAll.js
+++ js-exercises/sum-all/sumAll.js
@@ -1,5 +1,41 @@
-function sumAll() {
-  
+
+
+function sumAll(brr) {
+  let arr = [];
+  let inc = 0;
+  let incSec = 0;
+  let x = 0;
+
+  if(brr[0]>brr[1]){
+
+    for(let i=brr[0];i<=brr;i++){
+      arr[inc]= i;
+      inc = inc +1;
+    }
+    while(incSec <arr.length )
+    {
+    	x =  arr[incSec]+x;
+      incSec = incSec +1
+    }
+
+    return x ;
+
+
+  }else{
+
+    for(let i=brr[1];i<=brr[0];i++){
+      arr[inc]= i;
+      inc = inc +1;
+    }
+    while(incSec <arr.length )
+    {
+    	x =  arr[incSec]+x;
+      incSec = incSec +1
+    }
+
+    return x ;
+  }
+
 }
 
 export {
