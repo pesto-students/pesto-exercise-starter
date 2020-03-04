728c0ee514032092e064bb3d77696587d4e9ba06
shivam arora <shivamarora@live.com>
2020-03-04 19:24:31 +0530

    Minor fix in array utils

--- js-exercises/array-utils/arrayUtils.js
+++ js-exercises/array-utils/arrayUtils.js
@@ -1,35 +1,35 @@
-function forEach(array,callback){
-  if(typeof callback!='function') return ;
-  for(let index=0;index<array.length;index++){
-     callback(array[index],index,array)
+function forEach(array, callback) {
+  if (typeof callback != 'function') return;
+  for (let index = 0; index < array.length; index++) {
+    callback(array[index], index, array)
   }
 
 }
-function map(array,callback){
-  if(typeof callback!='function') return ;
-  let resultArr=[]
-  for(let index=0;index<array.length;index++){
-     resultArr.push(callback(array[index],index,array))
+function map(array, callback) {
+  if (typeof callback != 'function') return;
+  let resultArr = []
+  for (let index = 0; index < array.length; index++) {
+    resultArr.push(callback(array[index], index, array))
   }
   return resultArr
 }
-const filter=function(array,callbackFunction){
-  let filteredArr=[];
-  for(let index=0;index<array.length;index++){
+function filter(array, callback) {
+  let filteredArr = [];
+  for (let index = 0; index < array.length; index++) {
 
-      if(callbackFunction(array[index]),index,array)
-        filteredArr.push(array[index])
-    
+    if (callback(array[index]), index, array)
+      filteredArr.push(array[index])
+
   }
   return filteredArr
 }
-function reduce(array, callBack, initialValueOfAccumulator=array[0]) {
-  if(typeof callback!='function') return ;
-  let tempAccumulator =initialValueOfAccumulator;
-  for (let index = 0; index < array.length; i++) {
-    tempAccumulator = callBack(tempAccumulator, array[index], index, array);
+function reduce(array, callBack, initialAccumulator) {
+  let accumulator = initialAccumulator ? initialAccumulator : array[0];
+  let startIndex = initialAccumulator ? 0 : 1;
+  for (startIndex; startIndex < array.length; startIndex++) {
+    accumulator = callBack(accumulator, array[startIndex], startIndex, array);
   }
-  return tempAccumulator;
+  return accumulator;
 }
 export {
   forEach,
