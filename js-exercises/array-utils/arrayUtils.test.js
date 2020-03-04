92f0870ff72100f9d9167505c8954f0687827b0c
shivam arora <shivamarora@live.com>
2020-03-04 19:50:56 +0530

    Maths functions added

--- js-exercises/array-utils/arrayUtils.test.js
+++ js-exercises/array-utils/arrayUtils.test.js
@@ -2,7 +2,7 @@
 import { forEach, map, reduce, filter } from "./arrayUtils";
 describe("Use Array Utils", () => {
   it("should use array utility functions to get correct values", () => {
-    expect(map([1, 2, 3], element => element)).toStrictEqual([1, 2, 3]);
+    expect(map([5, 6, 7], element => element)).toStrictEqual([5, 6, 7]);
     expect(
       reduce(
         [1, 2, 3],
