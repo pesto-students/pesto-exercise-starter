b690e03443adfe6f04691a774542ca3442a331f7
anshul0888 <anshulsharma0888@gmail.com>
2020-03-04 16:48:00 +0530

    array Utils done

--- js-exercises/array-utils/arrayUtils.js
+++ js-exercises/array-utils/arrayUtils.js
@@ -1,7 +1,34 @@
-
-export {
-  forEach,
-  map,
-  filter,
-  reduce,
+function forEach(array, callBack) {
+  for (let i = 0; i < array.length; i++) {
+    callBack(array[i], i, array);
+  }
 }
+
+function map(array, callBack) {
+  const mappedArray = [];
+  for (let i = 0; i < array.length; i++) {
+    mappedArray.push(callBack(array[i], i, array));
+  }
+  return mappedArray;
+}
+
+function filter(array, callBack) {
+  const filteredArray = [];
+  for (let i = 0; i < array.length; i++) {
+    if (callBack(array[i], i, array)) {
+      filteredArray.push(array[i]);
+    }
+  }
+  return filteredArray;
+}
+
+function reduce(array, callBack, typeOfAccumulator = 0) {
+  let tempAccumulator = typeOfAccumulator;
+  for (let i = 0; i < array.length; i++) {
+    tempAccumulator = callBack(tempAccumulator, array[i], i, array);
+    console.log("hi, ", tempAccumulator);
+  }
+  return tempAccumulator;
+}
+
+export { forEach, map, filter, reduce };
--- js-exercises/array-utils/arrayUtils.test.js
+++ js-exercises/array-utils/arrayUtils.test.js
@@ -1,1 +1,16 @@
-// Write your own test cases.
+import { forEach, filter, map, reduce } from "./arrayUtils";
+
+describe("Use Array Utils functions", () => {
+  test("should use the array util functions to get correct values", () => {
+    // expect(forEach(["a", "b", "c"], element => {})).toBe();
+    expect(
+      filter(["a", "b", "c"], (element, index) => {
+        if (index === 1) {
+          return element;
+        }
+      })
+    ).toBe([]);
+    expect(map(["a", "b", "c"], element => element)).toBe(["a", "b", "c"]);
+    expect(reduce(["a", "b", "c"], element => {})).toBe(0);
+  });
+});
