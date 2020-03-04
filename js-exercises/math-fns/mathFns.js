fd58b8fd3816b8e796048825a0dcf1d0aced541b
Supratik Basu <mail.supratikbasu@gmail.com>
2020-03-04 19:58:38 +0530

    Feat(mathFns): Implemented mathfns function

---
+++ js-exercises/math-fns/mathFns.js
@@ -0,0 +1,11 @@
+// Don't change the export values.
+// You can use the function in the Math module
+const power = (number, exponent) => Math.pow(number, exponent)
+const round = (number) => Math.round(number)
+const sqrt = (number) => Math.sqrt(number)
+
+export {
+  sqrt,
+  power,
+  round,
+};
