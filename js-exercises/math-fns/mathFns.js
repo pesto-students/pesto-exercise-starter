53c28de478f96ae16c3eb931002b09f192dedfaf
Shadab <shadab14meb346@gmail.com>
2020-03-04 15:32:28 +0530

    added math-fns

---
+++ js-exercises/math-fns/mathFns.js
@@ -0,0 +1,10 @@
+// Don't change the export values.
+// You can use the function in the Math module
+const sqrt = (number) => Math.sqrt(number);
+const power = (base, power) => Math.pow(base, power);
+const round = (decimal) => Math.floor(decimal);
+export {
+  sqrt,
+  power,
+  round,
+};
