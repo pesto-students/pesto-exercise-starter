5867e3068909832d0dfc6dc28664e8ab4ec19578
prashant kumar singh <singh.prashantking@gmail.com>
2020-03-04 19:45:38 +0530

    curry function

--- js-exercises/curry/curry.js
+++ js-exercises/curry/curry.js
@@ -1,8 +1,13 @@
-
-function curry(){
-  
+function curry(func) {
+  return function curried(...firstArgs) {
+    if (firstArgs.length >= func.length) {
+      return func.apply(this, firstArgs);
+    } else {
+      return function(...secondArgs) {
+        return curried.apply(this, firstArgs.concat(secondArgs));
+      };
+    }
+  };
 }
 
-export {
-  curry,
-};
+export { curry };
