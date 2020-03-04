3fb5c4774d5e930e1de75b686d7a7d74586f0f1c
prashant kumar singh <singh.prashantking@gmail.com>
2020-03-04 23:13:26 +0530

    changes

--- js-exercises/cache-function/cacheFunction.js
+++ js-exercises/cache-function/cacheFunction.js
@@ -1,6 +1,13 @@
-function cacheFunction() {
+function cacheFunction(cb) {
+  const cache = {};
+  const invoker = (...args) => {
+    if (cb in cache) {
+      return cache[cb];
+    }
+    cache[cb] = cb(...args);
+    return cb(...args);
+  };
+  return invoker;
 }
 
-export {
-  cacheFunction,
-};
+export { cacheFunction };
--- js-exercises/limit-function-call-count/limitFunctionCallCount.js
+++ js-exercises/limit-function-call-count/limitFunctionCallCount.js
@@ -1,7 +1,7 @@
-const limitFunctionCallCount = () => {
-
+const limitFunctionCallCount = (cb, functionExecutionThreshold) => {
+  return function() {
+    return functionExecutionThreshold-- > 0 ? cb.apply(this, arguments) : null;
+  };
 };
 
-export {
-  limitFunctionCallCount,
-};
+export { limitFunctionCallCount };
