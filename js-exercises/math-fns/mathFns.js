3bfd8f9a0be47092404f545c9dda9c58321158f5
vishal gupta <vishal18.gupta@gmail.com>
2020-03-04 14:51:41 +0530

    math

---
+++ js-exercises/math-fns/mathFns.js
@@ -0,0 +1,13 @@
+
+// Don't change the export values.
+// You can use the function in the Math module
+
+const sqrt = (squareRootNumber) => Math.sqrt(squareRootNumber);
+const power = (powerNumber1, powerNumber2) => powerNumber1 ** powerNumber2;
+const round = (roundNumber1, roundNumber2) => Math.round(roundNumber1, roundNumber2);
+
+export {
+  sqrt,
+  power,
+  round,
+};
