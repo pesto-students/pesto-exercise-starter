2cd7311f73b5b7d324d6c6d0e4f9cbf6450b3874
Kashish <kashish@coalshastra.com>
2020-03-05 19:46:03 +0530

    day3_commit

---
+++ js-exercises/duplicate-letters/duplicateLetters.js
@@ -0,0 +1,31 @@
+
+// function duplicateLetters(...args) {
+  function duplicateLetters(str) {
+    let countObj = {};
+    let allUnique = true;
+  
+    for (let char of str) {
+      if (countObj[char]) {
+        allUnique = false;
+        countObj[char] += 1;
+      } else {
+        countObj[char] = 1;
+      }
+    }
+  
+    let max;
+  
+    if (allUnique) {
+      return false;
+    } else {
+      for (let char in countObj) {
+        max ? (max = Math.max(max, countObj[char])) : (max = countObj[char]);
+      }
+      return max;
+    }
+  }
+  let a = duplicateLetters("addssda");
+  console.log(a);
+  
+  module.export = { duplicateLetters };
+
---
+++ js-exercises/longest-word-in-string/longestWordInString.js
@@ -0,0 +1,13 @@
+function longestWordInString(string) {
+  const stringArray = string.split(' ');
+  let longestWordSize = 0;
+  for (let i = 0; i < stringArray.length; i++) {
+    if (stringArray[i].length > longestWordSize) {
+      longestWordSize = stringArray[i].length;
+    }
+  }
+  return longestWordSize;
+}
+var a = longestWordInString('May the force be with you');
+console.log(a);
+export default { longestWordInString };
