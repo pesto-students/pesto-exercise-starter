276b9197827ce296f5c5f2799336855194845570
gauravkr2506 <gauravkr2506@gmail.com>
2020-03-06 09:08:03 +0530

    Message

---
+++ js-exercises/diff-array/diffArray.js
@@ -0,0 +1,23 @@
+function diffArray(firstArray, secondArray) {
+  const firstUnique = firstArray.reduce((acc, el) => {
+    if (secondArray.indexOf(el) === -1) {
+      return [...acc, el];
+    }
+    return acc;
+  }, []);
+
+  const secondUnique = secondArray.reduce((acc, el) => {
+
+    if (firstArray.indexOf(el) === -1) {
+      return [...acc, el];
+    }
+    return acc;
+  }, []);
+
+  return firstUnique.concat(secondUnique);
+}
+
+
+module.exports = {
+  diffArray
+};
