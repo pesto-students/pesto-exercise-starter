f404625911f22d57b35fcab4c9bbe79ca8a20dd0
dikshant2929 <dikshant29@hotmail.com>
2020-03-09 01:28:31 +0530

    Check Triangle Validation
    A Short and simple program which takes input of array like [1,4] and which returns the sum of value from first to till next value in array

--- js-exercises/is-triangle/isTriangle.js
+++ js-exercises/is-triangle/isTriangle.js
@@ -1,7 +1,9 @@
-function isTriangle() {
-
+function isTriangle(sideFirst, sideSecond, sideThird) {
+  return (
+    sideFirst + sideSecond > sideThird
+    && sideFirst + sideThird > sideSecond
+    && sideSecond + sideThird > sideFirst
+  );
 }
 
-export {
-  isTriangle,
-};
+export { isTriangle };
