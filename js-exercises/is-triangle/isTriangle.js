f7ca247e076696b236379ff3fa326781eaf3c3df
Dinesh <dinesh25195@gmail.com>
2020-03-08 21:25:39 +0530

    solve is-triangle problem

--- js-exercises/is-triangle/isTriangle.js
+++ js-exercises/is-triangle/isTriangle.js
@@ -1,5 +1,10 @@
-function isTriangle() {
+function isTriangle(side1,side2,side3) {
 
+    if(side1 && side2 && side3){
+      return (((side1 + side2) > side3) && ((side2 + side3) > side2) && ((side3 + side1) > side2))
+    } 
+
+    return false;
 }
 
 export {
