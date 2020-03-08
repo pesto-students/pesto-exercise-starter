1b767a245c3988eb30c2ebe295ab99c98760b77a
Dinesh <dinesh25195@gmail.com>
2020-03-09 01:07:54 +0530

    solve sum-primes problem

--- js-exercises/sum-primes/sumPrimes.js
+++ js-exercises/sum-primes/sumPrimes.js
@@ -1,4 +1,40 @@
-function sumPrimes() {
+function sumPrimes(number) {
+
+    if(number && typeof number === 'number'){
+
+      let primes = [];
+      
+      if(number > 2){
+         primes.push(2);
+      }
+
+      for(let i = 3 ; i <= number ; ){
+
+        let isPrime = true;
+        
+        for(let j = 0 ; j < primes.length ; j++){
+
+           if(i % primes[j] === 0){
+              isPrime = false;
+           }
+
+        }
+
+        if(isPrime){
+          primes.push(i);
+        }
+
+        i += 2;
+
+      }
+
+      return primes.reduce((sum,prime) => {
+          return sum + prime;
+      });
+
+    } else {
+      return false;
+    }
 
 }
 
