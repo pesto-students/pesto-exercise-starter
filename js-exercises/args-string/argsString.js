c212a24b72defb098ae6b84302ada4bdc6d5ed05
prashant kumar singh <singh.prashantking@gmail.com>
2020-03-09 15:03:23 +0530

    args-string partially solve

--- js-exercises/args-string/argsString.js
+++ js-exercises/args-string/argsString.js
@@ -1,3 +1,13 @@
-const argsString = (message, args) => {};
+const argsString = (message, args) => {
+  return function(...values) {
+    let dict = values[values.length - 1] || {};
+    let result = [message[0]];
+    args.forEach(function(key, i) {
+      let value = Number.isInteger(key) ? values[key] : dict[key];
+      result.push(value, message[i + 1]);
+    });
+    return result.join("");
+  };
+};
 
 export { argsString };
