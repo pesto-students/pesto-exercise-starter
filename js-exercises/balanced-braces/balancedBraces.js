c410c9df3dfb83b147557b09f3da4b72f6107603
gunjankalita01 <gunjan.kalita@ekodus.com>
2020-03-10 10:54:06 +0530

    Update balance brace problem , remove  lint errors

--- js-exercises/balanced-braces/balancedBraces.js
+++ js-exercises/balanced-braces/balancedBraces.js
@@ -1,17 +1,19 @@
 
 function balancedBraces(...args) {
-  var input = args[0]
-  var braces_stack = []
-  for (var i = 0; i < input.length; i++) {
+  const input = args[0];
+  // eslint-disable-next-line camelcase
+  const braces_stack = [];
+  // eslint-disable-next-line vars-on-top
+  // eslint-disable-next-line no-plusplus
+  for (let i = 0; i < input.length; i++) {
     if (input[i] === '[' || input[i] === '{' || input[i] === '(') {
-      braces_stack.push(input[i])
+      braces_stack.push(input[i]);
     } else {
       if (input[i] === ']') {
         if (braces_stack[braces_stack.length - 1] === '[') {
           braces_stack.pop();
         } else {
-          console.log(braces_stack);
-          return false
+          return false;
         }
       }
 
@@ -19,8 +21,7 @@ function balancedBraces(...args) {
         if (braces_stack[braces_stack.length - 1] === '{') {
           braces_stack.pop();
         } else {
-          console.log(braces_stack);
-          return false
+          return false;
         }
       }
 
@@ -28,21 +29,16 @@ function balancedBraces(...args) {
         if (braces_stack[braces_stack.length - 1] === '(') {
           braces_stack.pop();
         } else {
-          console.log(braces_stack);
-          return false
+          return false;
         }
       }
-
     }
   }
   if (braces_stack.length === 0) {
-    return true
+    return true;
   }
- 
-  
   return false;
 }
-console.log(balancedBraces('for(i=0;i<x[i];i++){if(x[i]<10){b++;}else{b+=10;}}'));
 
 export {
   balancedBraces,
