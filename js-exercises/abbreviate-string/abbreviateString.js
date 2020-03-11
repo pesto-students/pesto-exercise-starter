a52d3dee4ce64c4b247b2f3c922bcc9d5ab8c48b
Kashish <kashish@coalshastra.com>
2020-03-11 17:49:49 +0530

    solutions clean code

---
+++ js-exercises/abbreviate-string/abbreviateString.js
@@ -0,0 +1,12 @@
+import { isString } from "util";
+
+function abbreviateString(str) {
+  if (!isString(str)) throw Error('Input should be a string.');
+  const words = str.split(' ');
+  const firstWord = words[0];
+  const abbreviatedLastWord = (words[words.length - 1][0]).toUpperCase();
+
+  return `${firstWord} ${abbreviatedLastWord}.`;
+}
+
+export { abbreviateString };
