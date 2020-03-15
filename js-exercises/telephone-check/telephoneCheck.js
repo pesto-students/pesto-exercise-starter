a57370476f5aac3da18188f51b299056ca8073c3
BhargavMantha <manthabhargav@gmail.com>
2020-03-15 21:07:10 +0530

    complete us-phone-validator
    compleated the us phone validator
    1.used a regx that tests the validity of the phone number
    2.the following is the url for the same
    <---------------url---------------->
    https://regex101.com/r/iZ2vL4/65
    <--------------url---------------->
    
    You can play around with the tool for working of the same
    :arrow_forward: :yum:
    felling lucky

---
+++ js-exercises/telephone-check/telephoneCheck.js
@@ -0,0 +1,22 @@
+/**
+ * Please be sincear and let me know about all the mistakes i have made
+ * please be a little kind as i am still a novoice and am still learning
+ *                                                              @Bhargav Mantha
+ *
+ * Procedure/Approach/psudo code i am following
+ * 1.cI constructed a regex to check if the given phone number is
+ * actually in the format of US Phone Number
+ * 2. If the phone number is iun the US phone number format return true else false
+ * @param {String} (Phonenumber)
+ */
+const regExMatcher = inputPhoneNumber => {
+  const regex = /^(1\s)?\(?\d{3}(\))?(-|\s)?\d{3}(-|\s)?\d{4}$/;
+  const pattern = new RegExp(regex);
+  const resultOfRegx = pattern.test(inputPhoneNumber);
+  return resultOfRegx;
+};
+function telephoneCheck(inputPhoneNumber) {
+  const resultOfReplacement = regExMatcher(inputPhoneNumber);
+  return resultOfReplacement;
+}
+export { telephoneCheck };
