51ca29d0861fbfd9216891bf9f59f70e2a47d9d1
BhargavMantha <manthabhargav@gmail.com>
2020-03-16 00:24:42 +0530

    complete abbrivatestring problem
    Abbrivate String problem completed
    
    @bhargavmantha
    
    the psudo code is written in the comments

--- js-exercises/rot-13/rot13.test.js
+++ js-exercises/rot-13/rot13.test.js
@@ -5,6 +5,8 @@ describe('rot13', () => {
     expect(rot13('SERR PBQR PNZC')).toEqual('FREE CODE CAMP');
     expect(rot13('SERR CVMMN!')).toEqual('FREE PIZZA!');
     expect(rot13('SERR YBIR?')).toEqual('FREE LOVE?');
-    expect(rot13('GUR DHVPX OEBJA QBT WHZCRQ BIRE GUR YNML SBK.')).toEqual('THE QUICK BROWN DOG JUMPED OVER THE LAZY FOX.');
+    expect(rot13('GUR DHVPX OEBJA QBT WHZCRQ BIRE GUR YNML SBK.')).toEqual(
+      'THE QUICK BROWN DOG JUMPED OVER THE LAZY FOX.'
+    );
   });
 });
