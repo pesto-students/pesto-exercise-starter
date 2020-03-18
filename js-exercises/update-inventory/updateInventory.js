f56c1e770931e4d39913b603f5878d2f3f1f7f92
prashant kumar singh <singh.prashantking@gmail.com>
2020-03-18 19:46:19 +0530

    solve update inventory question

--- js-exercises/update-inventory/updateInventory.js
+++ js-exercises/update-inventory/updateInventory.js
@@ -1,7 +1,34 @@
-function updateInventory(...args) {
-  return args;
-}
+function updateInventory(arr1, arr2) {
+  var index;
+  var getProductIndex = function(name) {
+    for (var i = 0; i < this.length; i++) {
+      if (this[i][1] === name) {
+        return i;
+      }
+    }
+    return undefined;
+  };
 
-export {
-  updateInventory,
-};
+  for (var i = 0; i < arr2.length; i++) {
+    index = getProductIndex.call(arr1, arr2[i][1]);
+
+    if (index === undefined) {
+      arr1.push(arr2[i]);
+    } else {
+      arr1[index][0] += arr2[i][0];
+    }
+  }
+
+  arr1.sort(function(a, b) {
+    if (a[1] > b[1]) {
+      return 1;
+    }
+    if (a[1] < b[1]) {
+      return -1;
+    }
+    return 0;
+  });
+
+  return arr1;
+}
+export { updateInventory };
