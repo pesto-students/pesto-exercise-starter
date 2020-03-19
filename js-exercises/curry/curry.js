85b7d3ded2d260d1eb5b803dbc5eeb576e716e93
Smriti <smriti@mulimobile.com>
2020-03-19 18:54:53 +0530

    day10: solutions - object-invert - boolean-expression-evaluator - curry

---
+++ js-exercises/curry/curry.js
@@ -0,0 +1,12 @@
+function curry(func) {
+  return function curried(...args) {
+    if (args.length >= func.length) {
+      return func.apply(this, args);
+    }
+    return (...args2) => curried.apply(this, args.concat(args2));
+  };
+}
+
+export {
+  curry,
+};
