0aef9fc45eb0319fc904feb501c00a08cd95d7b1
BhargavMantha <manthabhargav@gmail.com>
2020-03-26 14:51:34 +0530

    Update rangeIter.js
    compleated the rangeIter function

--- js-exercises/range-iter/rangeIter.js
+++ js-exercises/range-iter/rangeIter.js
@@ -1,3 +1,30 @@
-function rangeIter(lb, ub) {}
+function rangeIter(lb, ub) {
+  if (typeof ub === "undefined" || typeof lb === "undefined") {
+    throw new TypeError("undefined is not a number");
+  }
+  if (typeof ub === "string") {
+    throw new TypeError(`${ub} is not a number`);
+  }
+  if (typeof step == "undefined") {
+    var step;
+    step = 1;
+  }
+  if (ub === lb) {
+    return [lb];
+  }
+  if ((step > 0 && lb >= ub) || (step < 0 && lb <= ub)) {
+    return [];
+  }
+
+  const result = [];
+
+  for (let i = lb; step > 0 ? i <= ub : i > ub; i += step) {
+    result.push(i);
+  }
+
+  return result;
+}
+
+// console.log(rangeIter(-1, -1));
 
 export { rangeIter };
